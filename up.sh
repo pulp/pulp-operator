927846bbdefe9af576b821cb0a1ca976c02321a3
pulp <pulp@pulp-demo-g8.localdomain>
2019-04-25 15:48:15 -0400

    Add additional commands to up.sh

--- up.sh
+++ up.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# TODO: Check if all of these are needed.
+# TODO: Check if these should only ever be run once; or require
+# special logic to update
+kubectl create -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
+kubectl create -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
+kubectl apply -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
+
 kubectl create -f deploy/service_account.yaml
 kubectl create -f deploy/role.yaml
 kubectl create -f deploy/role_binding.yaml
