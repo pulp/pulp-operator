0eb988717ee89e8ebfe16e287e2186792aebbfdc
Mike DePaulo <mikedep333@redhat.com>
2019-04-26 17:22:51 -0400

    up.sh: Use declarative syntax

--- up.sh
+++ up.sh
@@ -3,11 +3,10 @@
 # TODO: Check if all of these are needed.
 # TODO: Check if these should only ever be run once; or require
 # special logic to update
-kubectl create -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
-kubectl create -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
+kubectl apply -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
 kubectl apply -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
 
-kubectl create -f deploy/service_account.yaml
-kubectl create -f deploy/role.yaml
-kubectl create -f deploy/role_binding.yaml
-kubectl create -f deploy/operator.yaml
+kubectl apply -f deploy/service_account.yaml
+kubectl apply -f deploy/role.yaml
+kubectl apply -f deploy/role_binding.yaml
+kubectl apply -f deploy/operator.yaml
