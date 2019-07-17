da4365549681bcb400437bd039c85b3a00512f0f
Mike DePaulo <mikedep333@redhat.com>
2019-07-17 14:53:24 -0400

    Use actual ports 24816 & 24817 as nodePorts
    Partially implements:
    ref #5136
    https://pulp.plan.io/issues/5136

--- .travis/k3s-install.sh
+++ .travis/k3s-install.sh
@@ -12,9 +12,12 @@
 # It will install the latest version.
 # We can always pass args to use a specific version or change other options.
 #
+# We want to allow devs to use Pulp's ports, like 80, 24816 or 24817,
+# not just the default 30000-32767.
+#
 # TODO: Fix access to registry.centos.org
 # https://github.com/rancher/k3s/issues/145#issuecomment-490143506
-curl -sfL https://get.k3s.io | sudo sh -
+curl -sfL https://get.k3s.io | sudo INSTALL_K3S_EXEC="--kube-apiserver-arg service-node-port-range=80-32767" sh -
 status=$(sudo systemctl status --full --lines=200 k3s)
 if ! [[ $? ]] ; then
   echo "${status}"
