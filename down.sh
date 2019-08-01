3eec12c95af299f431eef4a4633fd66bf776f8e1
Mike DePaulo <mikedep333@redhat.com>
2019-08-01 17:21:15 -0400

    Replace configMaps with better featured K8s Custom Resources
    Because ansible operators use them by default, they specifically
    watch for changes to them, and because the variable names are simpler.
    
    This commit includes:
    
    Making more options configurable
    
    Switching to the stable tag for the "pulp" image
    It (3.0.0rc4) actually contains drf 3.9.4, and therefore works.
    
    Let users easily define their own Custom Resources
    
    Rename the pulp image "version" to "tag"
    
    Note: Automatic camelCase conversion of CR field names necessitates
    using snake_case
    
    fixes: #5069
    containers: Replace configMap with better featured custom resources
    https://pulp.plan.io/issues/5219
    
    re: #5116
    Handle properly whether to build container images with pulpcore stable or dev
    https://pulp.plan.io/issues/5116

--- down.sh
+++ down.sh
@@ -5,13 +5,9 @@
 # Remove the containers/pods before everything they depend on.
 kubectl delete -f deploy/operator.yaml
 
-kubectl delete -f deploy/pulp-operator.default.config-map.yml
 kubectl delete -f deploy/service_account.yaml
 kubectl delete -f deploy/role.yaml
 kubectl delete -f deploy/role_binding.yaml
-
-# TODO: Check if all of these are needed.
-# TODO: Check if these should only ever be run once; or require
-# special logic to update
-kubectl delete -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
+# It doesn't matter which cr we specify; the metadata up top is the same.
+kubectl delete -f deploy/crds/pulpproject_v1alpha1_pulp_cr.default.yaml
 kubectl delete -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
