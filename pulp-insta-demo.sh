3da6a0b99f5c2bdf40f8963f8d1b2a461b69c5b7
Mike DePaulo <mikedep333@redhat.com>
2019-09-11 14:29:01 -0400

    pulp-insta-demo.sh: Eliminate duplicate code by calling updated k3s-install.sh
    Also some small changes to pulp-insta-demo.sh.

--- pulp-insta-demo.sh
+++ pulp-insta-demo.sh
@@ -31,20 +31,8 @@ if [[ $(getenforce || echo "Disabled") != "Disabled" ]]; then
   fi
 fi
 
-# This is their convenience installer script.
-# Does a bunch of stuff, such as setting up a `kubectl` -> `k3s kubectl` symlink.
-curl -sfL https://get.k3s.io | sudo INSTALL_K3S_EXEC="--kube-apiserver-arg service-node-port-range=80-32767" sh -
-sleep 30
-
-# By default, k3s lacks a storage class.
-# https://github.com/rancher/k3s/issues/85#issuecomment-468293334
-# This is the way to add a simple hostPath storage class.
-sudo kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
-# How make it the default StorageClass
-sudo kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-sudo kubectl cluster-info
-
-curl -L https://github.com/mikedep333/pulp-operator/archive/accomodate-insta-demo.tar.gz | tar -xz
+curl -SsL https://github.com/mikedep333/pulp-operator/archive/accomodate-insta-demo.tar.gz | tar -xz
 cd pulp-operator-accomodate-insta-demo
+sudo .travis/k3s-install.sh --insta-demo
 sudo TRAVIS=true ./up.sh
-.travis/pulp-operator-check-and-wait.sh
+.travis/pulp-operator-check-and-wait.sh || test $? = 100
