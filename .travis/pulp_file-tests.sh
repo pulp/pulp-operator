4a0902fa4ef024e6ba7025cbd574819216bfcff8
Mike DePaulo <mikedep333@redhat.com>
2019-10-09 16:22:40 -0400

    Problem: Travis CI is failing due to master branch pulp_file tests
    being run against stable release of pulp_file.
    
    Solution: Try the newest tag for pulp_file if the master branch fails.
    
    [noissue]

--- .travis/pulp_file-tests.sh
+++ .travis/pulp_file-tests.sh
@@ -11,6 +11,14 @@ pushd pulp_file/docs/_scripts
 # Let's only do sync tests.
 # So as to check that Pulp can work in containers, including writing to disk.
 # If the upload tests are simpler in the long run, just use them.
-set -x
-source docs_check_sync_publish.sh
+#
+# If the master branch tests fail, run the stable tests.
+# The git command is to checkout the newest stag, which should be the
+# stable release.
+# Temporary workaround until we replace with pulp-smash.
+timeout 5m bash -x docs_check_sync_publish.sh || {
+  echo "Master branch of pulp_file tests failed. Using newest tag (stable release.)"
+  git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+  timeout 5m bash -x docs_check_sync_publish.sh
+}
 
