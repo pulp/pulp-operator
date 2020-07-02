569caeb515da1a6fa5574b38891259fe78e4ecf7
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-07-02 16:45:52 -0300

    Fix quay push
    [noissue]

--- .travis/quay-push.sh
+++ .travis/quay-push.sh
@@ -9,12 +9,12 @@
 # Pulp is an organization (not an individual user account) on Quay:
 # https://quay.io/organization/pulp
 # For test publishes, one can override this to any org or user.
-QUAY_PROJECT_NAME=${QUAY_USER_OR_ORG_NAME:-pulp}
+QUAY_PROJECT_NAME=${QUAY_PROJECT_NAME:-pulp}
 # The image name, AKA the Quay repo
-QUAY_REPO_NAME=${QUAY_USER_OR_ORG_NAME:-pulp-operator}
+QUAY_REPO_NAME=${QUAY_REPO_NAME:-pulp-operator}
 # The image tag
-IMAGE_TAG=${QUAY_USER_OR_ORG_NAME:-latest}
+QUAY_IMAGE_TAG=${QUAY_IMAGE_TAG:-latest}
 
 # Reference: https://adriankoshka.github.io/blog/posts/travis-and-quay/
 echo "$QUAY_BOT_PASSWORD" | docker login -u "$QUAY_BOT_USERNAME" --password-stdin quay.io
-docker push "quay.io/$QUAY_PROJECT_NAME/$QUAY_REPO_NAME:$IMAGE_TAG"
+docker push "quay.io/$QUAY_PROJECT_NAME/$QUAY_REPO_NAME:$QUAY_IMAGE_TAG"
