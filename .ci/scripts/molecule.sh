34d04f10e16e5685f5614f898fa2bcc37e04f5f1
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-02-05 15:29:19 -0300

    Update Molecule tests
    [noissue]

--- .ci/scripts/molecule.sh
+++ .ci/scripts/molecule.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -e
 #!/usr/bin/env bash
 
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+
 # deploy/cluster_role_binding.yaml specify the namespace: default
 # The default namespace on molecule is: osdk-test
 # For running the molecule test we should:
 # - Option 1: change the namespace at deploy/cluster_role_binding.yaml to: osdk-test
 # - Option 2: change the namespace at molecule test to: default
 # As molecule provides TEST_OPERATOR_NAMESPACE we will use the 2nd option:
-export TEST_OPERATOR_NAMESPACE=default
+sed -i "s/default/osdk-test/g" deploy/cluster_role_binding.yaml
+
 
 echo "Starting molecule test"
-molecule test -s test-local
+molecule test -s kind
