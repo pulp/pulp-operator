38c3d883ffac1956fde7ff13e348190b91fb1cfa
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-08-08 17:52:33 -0300

    Build images on minikube docker-env
    [noissue]

--- .ci/scripts/deploy.sh
+++ .ci/scripts/deploy.sh
@@ -1,15 +1,24 @@
 #!/bin/bash -e
 #!/usr/bin/env bash
 
-DEPLOY_FLAG=""
-if
-  [ "$1" = "--index" ] || [ "$1" = "-i" ]; then
-  DEPLOY_FLAG="-i"
+if [[ "$CI_TEST" == "galaxy" ]]; then
+  echo "Deploy galaxy latest"
+  sudo -E QUAY_REPO_NAME=galaxy $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
+  echo "Deploy galaxy-web latest"
+  sudo -E QUAY_REPO_NAME=galaxy-web $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
+else
+  echo "Deploy pulp latest"
+  sudo -E QUAY_REPO_NAME=pulp $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
+
+  echo "Deploy pulpcore latest"
+  sudo -E QUAY_REPO_NAME=pulpcore $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
+
+  echo "Deploy pulp-web latest"
+  sudo -E QUAY_REPO_NAME=pulp-web $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
 fi
 
-if [[ "$DEPLOY_FLAG" == "-i" ]]; then
+if [[ -z "${QUAY_EXPIRE+x}" ]]; then
   echo "Deploy pulp-operator"
-  eval $(minikube -p minikube docker-env)
   sudo -E $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
   export QUAY_IMAGE_TAG=$(python -c 'import yaml; print(yaml.safe_load(open("deploy/olm-catalog/pulp-operator/manifests/pulp-operator.clusterserviceversion.yaml"))["spec"]["version"])')
   sed -i "s/\.dev//g" deploy/olm-catalog/pulp-operator/manifests/pulp-operator.clusterserviceversion.yaml
@@ -31,23 +40,6 @@ if [[ "$DEPLOY_FLAG" == "-i" ]]; then
   opm index add -c docker --bundles quay.io/pulp/pulp-operator-bundle:${QUAY_IMAGE_TAG} --tag quay.io/pulp/pulp-index:${QUAY_IMAGE_TAG}
   sudo -E QUAY_REPO_NAME=pulp-index $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
   docker images
-  exit
-fi
-
-if [[ "$CI_TEST" == "galaxy" ]]; then
-  echo "Deploy galaxy latest"
-  sudo -E QUAY_REPO_NAME=galaxy $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
-  echo "Deploy galaxy-web latest"
-  sudo -E QUAY_REPO_NAME=galaxy-web $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
-else
-  echo "Deploy pulp latest"
-  sudo -E QUAY_REPO_NAME=pulp $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
-
-  echo "Deploy pulpcore latest"
-  sudo -E QUAY_REPO_NAME=pulpcore $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
-
-  echo "Deploy pulp-web latest"
-  sudo -E QUAY_REPO_NAME=pulp-web $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
 fi
 
 docker images
