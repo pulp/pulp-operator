551900a7b7dca3f77c621fc7e7e1a316bde5487e
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-10-15 16:30:52 -0300

    Release 0.4.0
    [noissue]

--- .ci/scripts/deploy.sh
+++ .ci/scripts/deploy.sh
@@ -21,7 +21,7 @@ if [[ -z "${QUAY_EXPIRE+x}" ]]; then
   echo "Deploy pulp-operator"
   make docker-push
 
-  export QUAY_IMAGE_TAG=$(cat Makefile | grep "VERSION ?=" | cut -d' ' -f3)
+  export QUAY_IMAGE_TAG=v$(cat Makefile | grep "VERSION ?=" | cut -d' ' -f3)
   echo $QUAY_IMAGE_TAG
   docker tag quay.io/pulp/pulp-operator:devel quay.io/pulp/pulp-operator:$QUAY_IMAGE_TAG
   sudo -E $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
