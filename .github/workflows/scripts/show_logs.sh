72e3001eab3026328d455a1f55a3539e07470dd3
Christian M. Adams <chadams@redhat.com>
2021-10-25 14:13:48 -0400

    Rename operator image name to make it unique from resource & awx
    [noissue]
    
    Signed-off-by: Christian M. Adams <chadams@redhat.com>

--- .github/workflows/scripts/show_logs.sh
+++ .github/workflows/scripts/show_logs.sh
@@ -18,7 +18,7 @@ if [[ "$KUBE" == "minikube" ]]; then
 fi
 
 echo ::group::OPERATOR_LOGS
-sudo -E kubectl logs -l app.kubernetes.io/name=pulp-operator -c manager --tail=10000
+sudo -E kubectl logs -l app.kubernetes.io/name=pulp-operator -c pulp-manager --tail=10000
 echo ::endgroup::
 
 echo ::group::PULP_API_LOGS
