1b584ccd9663408cfcd4b4025f86d47d189c6400
Christian M. Adams <chadams@redhat.com>
2022-04-29 18:38:03 -0400

    Downstreamify 2.2 for release    * Add downstreamify.sh script to be run when cutting new downstream      branches from upstream    * Replace upstream refs with downstream names
    Change-Id: I6abfd23e30d9a79921a9f352ddbebc94caee479f

---
+++ downstreamify.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# -- General replaces section
+
+replacements=(
+
+    RELATED_IMAGE_PULP:RELATED_IMAGE_HUB
+    Pulp:AutomationHub
+    "AutomationHub 3":AutomationHub
+    pulp_pulpproject_org_pulp:automationhub_ansible_com_automationhub
+    pulp.pulpproject.org:automationhub.ansible.com
+)
+
+# Replace in roles files; settings configmap is intentionally left out to keep settings in tact
+for row in "${replacements[@]}"; do
+    upstream="$(echo $row | cut -d: -f1)";
+    downstream="$(echo $row | cut -d: -f2)";
+    find ./roles -type f -name '*' \
+      -not -path '*.md' \
+	    -exec sed -i -e "s/${upstream}/${downstream}/g" {} \;
+done
+
+# Replace in watches.yaml
+for row in "${replacements[@]}"; do
+    upstream="$(echo $row | cut -d: -f1)";
+    downstream="$(echo $row | cut -d: -f2)";
+    sed -i -e "s/${upstream}/${downstream}/g" ./watches.yaml ;
+done
+
+# -- Replace deployment_type
+
+for row in "${replacements[@]}"; do
+    upstream="$(echo $row | cut -d: -f1)";
+    downstream="$(echo $row | cut -d: -f2)";
+    sed -i -e "s/pulp/automationhub/g" ./roles/backup/vars/main.yml \
+                                        roles/backup/templates/event.yaml.j2 \
+                                        roles/pulp-worker/defaults/main.yml \
+                                        roles/restore/vars/main.yml ;
+done
+
+
+# -- Swap out postgres data path
+
+# Need to verify new path after this work lands for 2.2
+# sed -i -e "s/\/var\/lib\/pgsql\/data/\/var\/lib\/postgresql\/data\/pgdata/g" ./roles/postgres/defaults/main.yml
+
+# -- Inject RELATED_IMAGES_ references
+
+# Uneeded because it is still upstream
