89cc6d0aae89988b35eaf19ace29f55f83bd0ecf
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-05-13 14:23:01 +0100

    Revert "Fix kubelinter"
    This reverts commit f7e8f95508a3049745e2d6a4da051589fc5967cc.
    
    [noissue]

--- .ci/scripts/kubelinter.sh
+++ .ci/scripts/kubelinter.sh
@@ -7,13 +7,13 @@ RELEASE_INFO=$(curl --silent --show-error --fail https://api.github.com/repos/st
 RELEASE_NAME=$(echo "${RELEASE_INFO}" | jq --raw-output ".name")
 LOCATION=$(echo "${RELEASE_INFO}" \
   | jq --raw-output ".assets[].browser_download_url" \
-  | grep --fixed-strings kube-linter-linux)
-LINTER=kube-linter-linux-${RELEASE_NAME}
+  | grep --fixed-strings kube-linter-linux.tar.gz)
+TARGET=kube-linter-linux-${RELEASE_NAME}.tar.gz
 # Skip downloading release if downloaded already, e.g. when the action is used multiple times.
-if [ ! -e $LINTER ]; then
-  curl --silent --show-error --fail --location --output $LINTER "$LOCATION"
-  chmod +x $LINTER
+if [ ! -e $TARGET ]; then
+  curl --silent --show-error --fail --location --output $TARGET "$LOCATION"
+  tar -xf $TARGET
 fi
 mkdir -p lint
 sudo -E kubectl get pvc,configmap,serviceaccount,secret,networkpolicy,ingress,service,deployment,statefulset,hpa,job,cronjob -o yaml > ./lint/k8s-all.yaml
-./$LINTER lint ./lint --config .ci/assets/kubernetes/.kube-linter.yaml
+./kube-linter lint ./lint --config .ci/assets/kubernetes/.kube-linter.yaml
