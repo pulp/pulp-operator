49f76c632dc7acbf6046b87203edd1f66b3dd5c0
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-06-30 01:10:59 +0100

    Add readiness probe to content and workers
    closes #455

--- containers/images/pulp/container-assets/readyz.py
+++ containers/images/pulp/container-assets/readyz.py
@@ -3,22 +3,41 @@
 import os
 import sys
 import requests
+import socket
+from datetime import timedelta
 
-STATUS_PATH = sys.argv[1]
-print(f"Readiness probe checking {STATUS_PATH}")
-response = requests.get(f"http://localhost:24817{STATUS_PATH}", allow_redirects=True)
-data = response.json()
 
-if not data["online_workers"]:
-    sys.exit(1)
+def is_api_healthy(path):
+    """
+    Checks if API is healthy
+    """
+    print(f"Readiness probe checking {path}")
+    response = requests.get(
+        f"http://localhost:24817{path}", allow_redirects=True
+    )
+    data = response.json()
 
-if not data["online_content_apps"]:
-    sys.exit(2)
+    if not data["database_connection"]["connected"]:
+        sys.exit(3)
 
-if not data["database_connection"]["connected"]:
-    sys.exit(3)
+    if os.getenv("REDIS_SERVICE_HOST") and not data["redis_connection"]["connected"]:
+        sys.exit(4)
 
-if os.getenv("REDIS_SERVICE_HOST") and not data["redis_connection"]["connected"]:
-    sys.exit(4)
+    sys.exit(0)
 
-sys.exit(0)
+
+def is_content_healthy(path):
+    """
+    Checks if Content is healthy
+    """
+    print(f"Readiness probe checking {socket.gethostname()}")
+    response = requests.head(f"http://localhost:24816{path}")
+    response.raise_for_status()
+
+    sys.exit(0)
+
+if os.getenv("PULP_API_WORKERS"):
+    is_api_healthy(sys.argv[1])
+
+elif os.getenv("PULP_CONTENT_WORKERS"):
+    is_content_healthy(sys.argv[1])
