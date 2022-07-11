100cea441ffd737f7a43bf51039373d976793412
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-07-11 15:06:59 +0100

    Fix pulp images deploy
    [noissue]

--- .github/workflows/scripts/test_images.sh
+++ .github/workflows/scripts/test_images.sh
@@ -22,7 +22,7 @@ if [[ -n "${QUAY_EXPIRE}" ]]; then
   echo "LABEL quay.expires-after=${QUAY_EXPIRE}d" >> ./build/Dockerfile
 fi
 sudo -E ./up.sh
-$KUBECTL wait --for condition=Pulp-Operator-Finished-Execution pulp/example-pulp --timeout=600s
+time $KUBECTL wait --for condition=Pulp-Operator-Finished-Execution pulp/example-pulp --timeout=-1s
 if [[ "$CI_TEST" == "galaxy" ]]; then
   CI_TEST=true .ci/scripts/galaxy_ng-tests.sh -m
 else
