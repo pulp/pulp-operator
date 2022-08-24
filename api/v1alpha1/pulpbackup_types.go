adfbf004711b35aa94166846311bd694f6d84429
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-08-24 02:49:45 +0100

    Adding OLM descriptors
    https://sdk.operatorframework.io/docs/building-operators/golang/references/markers/
    
    [noissue]

--- api/v1alpha1/pulpbackup_types.go
+++ api/v1alpha1/pulpbackup_types.go
@@ -22,76 +22,98 @@ import (
 
 // PulpBackupSpec defines the desired state of PulpBackup
 type PulpBackupSpec struct {
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	DeploymentType string `json:"deployment_type"`
 
 	// Name of the deployment to be backed up
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	DeploymentName string `json:"deployment_name"`
 
 	// +kubebuilder:default:="pulp"
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	InstanceName string `json:"instance_name"`
 
 	// Name of the PVC to be used for storing the backup
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	BackupPVC string `json:"backup_pvc"`
 
 	// Namespace PVC is in
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	BackupPVCNamespace string `json:"backup_pvc_namespace"`
 
 	// Storage requirements for the backup
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	BackupStorageReq string `json:"backup_storage_requirements"`
 
 	// Storage class to use when creating PVC for backup
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors={"urn:alm:descriptor:io.kubernetes:StorageClass"}
 	BackupSC string `json:"backup_storage_class"`
 
 	// Label selector used to identify postgres pod for executing migration
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	PostgresLabelSelector string `json:"postgres_label_selector"`
 
 	// Secret where the administrator password can be found
 	// +kubebuilder:default:="pulp-admin-password"
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors={"urn:alm:descriptor:io.kubernetes:Secret"}
 	AdminPasswordSecret string `json:"admin_password_secret,omitempty"`
 
 	// Secret where the database configuration can be found
 	// +kubebuilder:default:="pulp-postgres-configuration"
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Database configuration"
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors={"urn:alm:descriptor:io.kubernetes:Secret"}
 	PostgresConfigurationSecret string `json:"postgres_configuration_secret"`
 }
 
 // PulpBackupStatus defines the observed state of PulpBackup
 type PulpBackupStatus struct {
+	//+operator-sdk:csv:customresourcedefinitions:type=status,xDescriptors={"urn:alm:descriptor:io.kubernetes.conditions"}
 	Conditions []metav1.Condition `json:"conditions"`
 
 	// Name of the deployment backed up
+	//+operator-sdk:csv:customresourcedefinitions:type=status
 	DeploymentName string `json:"deploymentName"`
 
 	// The PVC name used for the backup
+	//+operator-sdk:csv:customresourcedefinitions:type=status
 	BackupClaim string `json:"backupClaim"`
 
 	// The namespace used for the backup claim
+	//+operator-sdk:csv:customresourcedefinitions:type=status
 	BackupNamespace string `json:"backupNamespace"`
 
 	// The directory data is backed up to on the PVC
+	//+operator-sdk:csv:customresourcedefinitions:type=status
 	BackupDirectory string `json:"backupDirectory"`
 
 	// The deployment storage type
+	//+operator-sdk:csv:customresourcedefinitions:type=status
 	DeploymentStorageType string `json:"deploymentStorageType"`
 
 	// Administrator password secret used by the deployed instance
+	//+operator-sdk:csv:customresourcedefinitions:type=status
 	AdminPasswordSecret string `json:"adminPasswordSecret"`
 
 	// Database configuration secret used by the deployed instance
+	//+operator-sdk:csv:customresourcedefinitions:type=status
 	DatabaseConfigSecret string `json:"databaseConfigurationSecret"`
 
 	// Objectstorage configuration secret used by the deployed instance
+	//+operator-sdk:csv:customresourcedefinitions:type=status
 	StorageSecret string `json:"storageSecret"`
 
 	// DB fields encryption configuration secret used by deployed instance
+	//+operator-sdk:csv:customresourcedefinitions:type=status
 	DBFieldsEncryptionSecret string `json:"dbFieldsEncryptionSecret"`
 
 	// Container token configuration secret used by the deployed instance
+	//+operator-sdk:csv:customresourcedefinitions:type=status
 	ContainerTokenSecret string `json:"containerTokenSecret"`
 }
 
