1066bbb0c4cfbb71908dc2b62fd88551f69e59b0
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-08-31 18:34:27 +0100

    Minor refactor on containerExec
    [noissue]

--- controllers/utils.go
+++ controllers/utils.go
@@ -1,8 +1,16 @@
 package controllers
 
 import (
+	"bytes"
+	"strings"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
@@ -37,4 +45,45 @@ func IsOpenShift() (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+
+func ContainerExec(client rest.Interface, scheme *runtime.Scheme, config *rest.Config, pod *corev1.Pod, command []string, container, namespace string) (string, error) {
+	execReq := client.
+		Post().
+		Namespace(namespace).
+		Resource("pods").
+		Name(pod.Name).
+		SubResource("exec").
+		VersionedParams(&corev1.PodExecOptions{
+			Container: container,
+			Command:   command,
+			Stdout:    true,
+			Stderr:    true,
+		}, runtime.NewParameterCodec(scheme))
+
+	exec, err := remotecommand.NewSPDYExecutor(config, "POST", execReq.URL())
+	if err != nil {
+		return "", err
+	}
+
+	stdout := new(bytes.Buffer)
+	stderr := new(bytes.Buffer)
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdout: stdout,
+		Stderr: stderr,
+		Tty:    false,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	result := strings.TrimSpace(stdout.String()) + "\n" + strings.TrimSpace(stderr.String())
+	result = strings.TrimSpace(result)
+
+	// [TODO] remove this sleep and find a better way to make sure that it finished execution
+	// I think the exec.Stream command is not synchronous and sometimes when a task depends
+	// on the results of the previous one it is failing.
+	// But this is just a guess!!! We need to investigate it further.
+	time.Sleep(time.Second)
+	return result, nil
 }
