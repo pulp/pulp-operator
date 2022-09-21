c17001d735590302703bf8127f4889d11a82d23a
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-09-21 15:48:40 +0100

    Fix deploy job
    [noissue]

--- .ci/scripts/deploy.sh
+++ .ci/scripts/deploy.sh
@@ -1,21 +1,19 @@
 #!/bin/bash -e
 #!/usr/bin/env bash
 
-if [[ -z "${QUAY_EXPIRE+x}" ]]; then
-  echo "Deploy pulp-operator"
-  make docker-push
+QUAY_BOT_USERNAME=${QUAY_BOT_USERNAME:-pulp+github}
 
-  export QUAY_IMAGE_TAG=v$(cat Makefile | grep "VERSION ?=" | cut -d' ' -f3)
-  echo $QUAY_IMAGE_TAG
-  docker tag quay.io/pulp/pulp-operator:devel quay.io/pulp/pulp-operator:$QUAY_IMAGE_TAG
-  sudo -E $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
+echo "$QUAY_BOT_PASSWORD" | docker login -u "$QUAY_BOT_USERNAME" --password-stdin quay.io
+make docker-push
 
-  make bundle-build
-  make bundle-push
+export QUAY_IMAGE_TAG=v$(cat Makefile | grep "VERSION ?=" | cut -d' ' -f3)
+echo $QUAY_IMAGE_TAG
+docker tag quay.io/pulp/pulp-operator:devel quay.io/pulp/pulp-operator:$QUAY_IMAGE_TAG
+sudo -E $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
 
-  make catalog-build
-  make catalog-push
-  docker images
-fi
+make bundle-build
+make bundle-push
 
+make catalog-build
+make catalog-push
 docker images
