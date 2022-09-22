cf0750f9ad7814cb2f8d87cc92a5468b74dfe9c7
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-09-22 19:05:50 +0100

    Ensure CI logs
    [noissue]

--- .github/workflows/scripts/show_logs.sh
+++ .github/workflows/scripts/show_logs.sh
@@ -10,6 +10,20 @@ if [[ "$1" == "--kind" ]] || [[ "$1" == "-k" ]]; then
   kind export kubeconfig --name osdk-test
   sudo -E kubectl config set-context --current --namespace=osdk-test
   sudo -E kubectl port-forward service/example-pulp-web-svc 24880:24880 &
+  echo ::group::KIND_LOGS
+  mkdir -p logs
+  kind export logs --name osdk-test ./logs
+  cd logs
+  cat docker-info.txt
+  cat kind-version.txt
+  cd osdk-test-control-plane
+  cat alternatives.log
+  cat containerd.log
+  cat inspect.json | jq
+  cat journal.log
+  cat kubelet.log
+  cat kubernetes-version.txt
+  echo ::endgroup::
 fi
 
 sudo -E kubectl get pods -o wide
@@ -31,27 +45,27 @@ sudo -E kubectl logs -l app.kubernetes.io/name=pulp-operator -c pulp-manager --t
 echo ::endgroup::
 
 echo ::group::PULP_API_LOGS
-sudo -E kubectl logs -l app.kubernetes.io/name=pulp-api --tail=10000
+sudo -E kubectl logs -l app.kubernetes.io/name=pulp-api --tail=10000 || true
 echo ::endgroup::
 
 echo ::group::PULP_CONTENT_LOGS
-sudo -E kubectl logs -l app.kubernetes.io/name=pulp-content --tail=10000
+sudo -E kubectl logs -l app.kubernetes.io/name=pulp-content --tail=10000 || true
 echo ::endgroup::
 
 echo ::group::PULP_WORKER_LOGS
-sudo -E kubectl logs -l app.kubernetes.io/name=pulp-worker --tail=10000
+sudo -E kubectl logs -l app.kubernetes.io/name=pulp-worker --tail=10000 || true
 echo ::endgroup::
 
 echo ::group::PULP_WEB_LOGS
-sudo -E kubectl logs -l app.kubernetes.io/name=nginx --tail=10000
+sudo -E kubectl logs -l app.kubernetes.io/name=nginx --tail=10000 || true
 echo ::endgroup::
 
 echo ::group::POSTGRES
-sudo -E kubectl logs -l app.kubernetes.io/name=postgres --tail=10000
+sudo -E kubectl logs -l app.kubernetes.io/name=postgres --tail=10000 || true
 echo ::endgroup::
 
 echo ::group::EVENTS
-sudo -E kubectl get events --sort-by='.metadata.creationTimestamp'
+sudo -E kubectl get events --sort-by='.metadata.creationTimestamp' || true
 echo ::endgroup::
 
 echo ::group::OBJECTS
