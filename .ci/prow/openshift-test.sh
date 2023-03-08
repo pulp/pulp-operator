8cb826c49cbe8df8c4ac286a304a2e474056d1a5
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2023-03-08 08:00:10 -0300

    Keep showing logs in case of error in prow script
    [noissue]

--- .ci/prow/openshift-test.sh
+++ .ci/prow/openshift-test.sh
@@ -9,17 +9,17 @@ show_logs() {
   oc get pods -o wide
   oc get routes -o wide
   echo "======================== Operator ========================"
-  oc logs -l app.kubernetes.io/name=pulp-operator -c manager --tail=10000
+  oc logs -l app.kubernetes.io/name=pulp-operator -c manager --tail=10000 || true
   echo "======================== API ========================"
-  oc logs -l app.kubernetes.io/name=pulp-api --tail=10000
+  oc logs -l app.kubernetes.io/name=pulp-api --tail=10000 || true
   echo "======================== Content ========================"
-  oc logs -l app.kubernetes.io/name=pulp-content --tail=10000
+  oc logs -l app.kubernetes.io/name=pulp-content --tail=10000 || true
   echo "======================== Worker ========================"
-  oc logs -l app.kubernetes.io/name=pulp-worker --tail=10000
+  oc logs -l app.kubernetes.io/name=pulp-worker --tail=10000 || true
   echo "======================== Postgres ========================"
-  oc logs -l app.kubernetes.io/name=postgres --tail=10000
+  oc logs -l app.kubernetes.io/name=postgres --tail=10000 || true
   echo "======================== Events ========================"
-  oc get events --sort-by='.metadata.creationTimestamp'
+  oc get events --sort-by='.metadata.creationTimestamp' || true
   echo "======================== CR ========================"
   oc get pulp -oyaml
   exit 1
