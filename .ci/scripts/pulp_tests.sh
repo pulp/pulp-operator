48894b2ff870206733c6900d4ddd02db0b860421
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2023-04-03 14:46:26 -0300

    Fix reconciliation and pipeline issues
    The operator execution was not converging because of multiple tasks
    modifying the same resource in different ways.
    ref: https://issues.redhat.com/browse/AAP-7866
    [noissue]

--- .ci/scripts/pulp_tests.sh
+++ .ci/scripts/pulp_tests.sh
@@ -24,12 +24,7 @@ password password\
 export BASE_ADDR="http://$SERVER:$WEB_PORT"
 echo $BASE_ADDR
 
-# Install pulp-cli the same way we do in pulp_ansible repo (to avoid tests with version mismatch, for example)
-# https://github.com/pulp/pulp_ansible/blob/5778d86ae51738578f7c5f00214b5ccb8aa1ee45/.github/workflows/scripts/before_install.sh#L96-L102
-git clone --depth=1 https://github.com/pulp/pulp-cli.git
-pushd pulp-cli
-pip install .
-popd
+pip install pulp-cli
 
 if [ ! -f ~/.config/pulp/settings.toml ]; then
   echo "Configuring pulp-cli"
