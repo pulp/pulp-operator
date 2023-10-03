9ac0dc13ce985e2cd869ab83f1d8a02e918105bc
Dennis Kliban <dkliban@redhat.com>
2023-10-03 16:06:30 -0400

    WIP: deploy the operator to the ephemeral env
    Tries to setup bonfire.

--- pr_check.sh
+++ pr_check.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/cicd-tools/master
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+make sdkbin OPERATOR_SDK_VERSION=v1.29.0 LOCALBIN=/tmp
