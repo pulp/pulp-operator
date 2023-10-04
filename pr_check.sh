33b0163ccb8d3e730cd0b3c7a1abb4c9f57ba655
Dennis Kliban <dkliban@redhat.com>
2023-10-04 11:38:17 -0400

    more debugging commands

--- pr_check.sh
+++ pr_check.sh
@@ -22,4 +22,6 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 echo "WORKING DIR:"
 pwd
 cd pulp-operator
+pwd
+ls -la
 make sdkbin OPERATOR_SDK_VERSION=v1.29.0 LOCALBIN=/tmp
