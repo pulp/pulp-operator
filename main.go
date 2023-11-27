fd3291ed6b71895c6995f8bbc378d3f4fd1f2dc3
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2023-11-27 14:10:03 -0300

    Update pulp-server import from django-auth-ldap
    fixes: #1154

--- main.go
+++ main.go
@@ -170,7 +170,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	setupLog.Info("pulp-operator version: 1.0.10-beta.3")
+	setupLog.Info("pulp-operator version: 1.0.11-beta.3")
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
