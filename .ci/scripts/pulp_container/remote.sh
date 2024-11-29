fa7ec8327aa38a4033fd6e99f8257646e03d58ae
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2024-11-29 13:32:39 -0300

    Change the container registry to ghcr.io
    Modify the test script from dockerhub to ghcr to avoid the
    "429 Too Many Requests" issue.
    [noissue]

--- .ci/scripts/pulp_container/remote.sh
+++ .ci/scripts/pulp_container/remote.sh
@@ -4,7 +4,7 @@ REMOTE_NAME=$(head /dev/urandom | tr -dc a-z | head -c5)
 echo "Creating $REMOTE_NAME remote that points to an external source of container images."
 REMOTE_HREF=$(http POST $BASE_ADDR/pulp/api/v3/remotes/container/container/ \
     name=$REMOTE_NAME \
-    url='https://registry-1.docker.io' \
+    url='https://ghcr.io' \
     upstream_name='pulp/test-fixture-1' | jq -r '.pulp_href')
 
 echo "Inspecting new Remote."
