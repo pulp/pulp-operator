bf6099164aa570c87fffe8f97241ae02c633c144
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2026-02-28 10:17:12 -0300

    Update postgres in config samples and bkp/restore
    This commit fix some issues in CI, but mainly the postgres version
    incompatibility with django 5.
    
    Update postgres to 15 in CR samples, bkp and restore controllers.
    This is a workaround to be able to make the CI green and publish
    version 1.3.0 while we work on the issue with postgres 13 upgrade.
    
    [noissue]

--- .ci/scripts/pulp_ansible/remote-collection.sh
+++ .ci/scripts/pulp_ansible/remote-collection.sh
@@ -1,7 +1,7 @@
 # Create a remote that syncs some versions of django into your repository.
 pulp ansible remote -t "collection" create \
     --name "cbar" \
-    --url "https://galaxy-dev.ansible.com/" \
-    --requirements $'collections:\n  - testing.ansible_testing_content'
+    --url "https://galaxy.ansible.com/" \
+    --requirements $'collections:\n  - ansible.galaxy_collection'
 # If requirements are in a file
 # you can use the option '--requirements @<file_name>' instead.
